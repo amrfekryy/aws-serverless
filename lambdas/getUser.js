e7a7c04ba9a625e91a774ef5c73fd84a0bb0e50e
Amr Fekry <amrfekryy@gmail.com>
2021-10-01 17:09:40 +0200

    add and sync s3, create first lambda

---
+++ lambdas/getUser.js
@@ -0,0 +1,22 @@
+const Responses = require('./API_Responses')
+
+exports.handler = async event => {
+    console.log('even', event)
+
+    const {pathParameters} = event
+
+    if(!pathParameters || !pathParameters.ID) {
+        return Responses._400({message: 'Missing the ID from the path'})
+    }
+    if(data[pathParameters.ID]) {
+        return Responses._200(data[pathParameters.ID])
+    }
+    return Responses._400({message: 'No ID in data'})
+}
+
+const data = {
+    1: {name: 'Amr', age: 12, job: 'Developer'},
+    2: {name: 'Amr', age: 12, job: 'Developer'},
+    3: {name: 'Amr', age: 12, job: 'Developer'},
+    4: {name: 'Amr', age: 12, job: 'Developer'},
+}
