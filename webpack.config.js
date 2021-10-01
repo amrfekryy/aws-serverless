725b229257d53a709b2539827888620f0df55c5b
Amr Fekry <amrfekryy@gmail.com>
2021-10-01 17:37:40 +0200

    use webpack to minify code, and only deploy lambda code for each lamda

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    target: 'node',
+    mode: 'production', // minify code to 1 line and smallest size possible.
+    // mode: 'none', // minify code to 1 file (helpful for locating errors in the code)
+}
